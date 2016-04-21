cd61b6378a21ac6b55a915a815c8e4662b82ba66
freddiecabrera <freddiecabrera18@yahoo.com>
2016-04-20 23:58:55 -0700

    create test for updating DOM when props change

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils'
 import Voting from '../../src/components/Voting'
 import { expect } from 'chai'
+import { List } from 'immutable'
 
 describe( 'Voting', () => {
 
@@ -52,5 +53,32 @@ describe( 'Voting', () => {
   expect(winner).to.be.ok
   expect(winner.textContent).to.contain('Friday')
  })
+
+ it( 'renders as a pure component', () => {
+  const pair = ['Friday', 'Next Friday']
+  const container = document.createElement('div')
+  let component = ReactDOM.render( <Voting pair={pair} />, container )
+  let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+  expect(firstButton.textContent).to.equal('Friday')
+
+  pair[0] = 'Friday Afrer Next'
+  component = ReactDOM.render( <Voting pair={pair} />, container)
+  firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+  expect(firstButton.textContent).to.equal('Friday')
+ })
+
+ it( 'does update DOM when prop changes', () => {
+   const pair = List.of('Friday', 'Next Friday')
+   const container = document.createElement('div')
+   let component = ReactDOM.render( <Voting pair={pair} />, container )
+
+   let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+   expect(firstButton.textContent).to.equal('Friday')
+
+   const newPair = pair.set(0, 'Friday After Next')
+   component = ReactDOM.render( <Voting pair={newPair} />, container );
+   firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+   expect(firstButton.textContent).to.equal('Friday After Next')
+  })
 
 })
